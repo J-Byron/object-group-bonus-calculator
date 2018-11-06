8c00e862604a6e21b0946a397e6bc579935e7f16
Josh Byron <joshhbyron@gmail.com>
2018-11-06 16:31:14 -0600

    Completed assignment

--- client.js
+++ client.js
@@ -25,3 +25,72 @@ const employees = [ atticus, jem, scout, robert, mayella ]; // this is an array 
 // Ask questions when you don't.
 
 console.log( employees );
+
+/*
+
+Loop over the employees array and do the following:
+
+use each Employee object as the input to the function described below.
+console.log the results of each iteration.
+*/
+
+
+function newEmployeeInfo(employee){
+  let newEmployeeInfo = {
+    name: employee.name,
+    bonusPercentage: getBonusPercentage(employee.reviewRating,employee.annualSalary, employee.employeeNumber),
+    totalCompensation: parseInt(employee.annualSalary) + (parseInt(employee.annualSalary) * getBonusPercentage(employee.reviewRating,employee.annualSalary, employee.employeeNumber)),
+    totalBonus: Math.floor(parseInt(employee.annualSalary) * getBonusPercentage(employee.reviewRating,employee.annualSalary, employee.employeeNumber))  }
+
+    console.log('');
+    
+    return newEmployeeInfo; 
+}
+
+function getBonusPercentage(rating,income,employeeNumber){
+  let bonus = 0;
+
+
+
+  if(rating == 3){
+    bonus = 0.04;
+    
+  }
+  else if(rating == 4){
+    bonus = 0.06;
+  }
+  else if(rating == 5){
+    bonus = 0.10;
+  }
+
+  if(employeeNumber.length == 4){
+    bonus += .05;
+  }
+  if(income >= 65000 ){
+    bonus -= .01
+  }
+
+  if(bonus > .13){
+    bonus = .13;
+  }
+  if(bonus < 0){
+    bonus = 0;
+  }
+  return bonus;
+}
+
+/*
+Put the output on the DOM (visually on the page).
+Make the app run only after the user clicks on a button on the page
+Then style the output, making it visually appealing
+*/
+
+$(function(){
+  $('#startButton').on('click',function(){
+    for (let employee of employees) {
+      let newInfo = newEmployeeInfo(employee);
+      console.log(newInfo);
+      $(`body`).append(`<h2 class = employee> [Name]: ${newInfo.name} [Bonus Percentage]: ${newInfo.bonusPercentage}  [Total Compensation]: ${newInfo.totalCompensation} [Total Bonus] ${newInfo.totalBonus} </h2>`);
+    }
+  })
+})
